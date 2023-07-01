3d4ae94d738fd6a1c8202c1c0cec67e155142fc3
unknown <theultimategamingspot@gmail.com>
2023-07-01 20:57:44 +0530

    Updated Tickets - They Work Better Now

--- bot/extensions/tasks.py
+++ bot/extensions/tasks.py
@@ -9,11 +9,12 @@ MONGODB = os.getenv("MONGODB")
 
 class Tasks(commands.Cog):
 
-    def __init__(self, bot) -> None:
+    def __init__(self, bot:commands.Bot) -> None:
         self.bot = bot
         self.cluster = MongoClient(MONGODB)
         self.db = self.cluster["foox"]
         self.bans = self.db["bans"]
+        self.tickets = self.db["tickets"]
 
     @tasks.loop(seconds=10.0)
     async def _unban(self):
@@ -34,6 +35,29 @@ class Tasks(commands.Cog):
 
     @_unban.before_loop
     async def _before_unban(self):
+        await self.bot.wait_until_ready()
+
+    @tasks.loop(seconds=10.0)
+    async def _ticket_check(self):
+        cursor = self.tickets.find({})
+        for document in cursor:
+            if document["user_left"] or document["status"] == "CLOSED":
+                return
+
+            guild_id = document["guild_id"]
+            channel_id = document["channel_id"]
+            user_id = document["user_id"]
+            
+            guild = await self.bot.fetch_guild(guild_id)
+            channel = await guild.fetch_channel(channel_id)
+            user = await guild.fetch_member(user_id)
+
+            if user == None:
+                await channel.send("The user who opened this ticket has left.")
+                self.tickets.update_one(document, {"$set": {"user_left": True}})
+    
+    @_ticket_check.before_loop
+    async def _before_ticket_check(self):
         await self.bot.wait_until_ready()
 
 async def setup(bot:commands.Bot) -> None:
--- bot/extensions/tickets.py
+++ bot/extensions/tickets.py
@@ -1,43 +1,93 @@
-import discord, asyncio
+import discord, asyncio, pathlib, os
 from discord.ext import commands
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path=pathlib.Path("./.env"))
+
+MONGODB = os.getenv("MONGODB")
 
 class ReasonModal(discord.ui.Modal, title="Close"):
     answer = discord.ui.TextInput(label = "Reason", style = discord.TextStyle.paragraph, placeholder="Reason for closing the ticket. Eg:- Resolved", required=True)
+    cluster = MongoClient(MONGODB)
+    db = cluster["foox"]
+    tickets = db["tickets"]
+
 
     async def on_submit(self, interaction:discord.Interaction):
+        ticket_data = self.tickets.find_one({"channel_id": interaction.channel.id})
+        user_id = ticket_data["user_data"]
+        user_left = ticket_data["user_left"]
+        if not user_left:
+            user = await interaction.guild.fetch_member(user_id)
         channel = interaction.guild.get_channel(1124607726530609214)
         embed = discord.Embed(title="Ticket Closed", colour=0xADD8E6)
-        embed.add_field(name=f"Opened By", value=f"{user.mention}", inline=True)
-        embed.add_field(name=f"Closed By", value=f"{interaction.user.mention}", inline=True)
-        embed.add_field(name=f"Reason", value=f"{self.answer}", inline=True)
-        embed.set_thumbnail(url=user.avatar.url)
+
+        if user_left:
+            embed.add_field(name=f"Opened By", value=f"{user_id} - User Left", inline=True)
+            embed.add_field(name=f"Closed By", value=f"{interaction.user.mention}", inline=True)
+            embed.add_field(name=f"Reason", value=self.answer, inline=True)
+        else:
+            try:
+                embed.add_field(name=f"Opened By", value=f"{user.mention}", inline=True)
+                embed.add_field(name=f"Closed By", value=f"{interaction.user.mention}", inline=True)
+                embed.add_field(name=f"Reason", value=self.answer, inline=True)
+                embed.set_thumbnail(url=user.avatar.url)
+            except:
+                return await interaction.response.send_message("Something went wrong while closing this ticket...")
+        
         await channel.send(embed=embed)
 
+        self.tickets.update_one(ticket_data, {"$set": {"reason": self.answer, "status": "CLOSED"}})
+
         await interaction.response.send_message("Closing..")
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.5)
         await interaction.channel.delete()
 
 class CloseTicket(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
+        self.cluster = MongoClient(MONGODB)
+        self.db = self.cluster["foox"]
+        self.tickets = self.db["tickets"]
     
     @discord.ui.button(label="Close", custom_id="close", style=discord.ButtonStyle.red)
     async def _close(self, interaction:discord.Interaction, button:discord.ui.Button):
+        ticket_data = self.tickets.find_one({"channel_id": interaction.channel.id})
+        user_id = ticket_data["user_data"]
+        user_left = ticket_data["user_left"]
+        if not user_left:
+            user = await interaction.guild.fetch_member(user_id)
         channel = interaction.guild.get_channel(1124607726530609214)
         embed = discord.Embed(title="Ticket Closed", colour=0xADD8E6)
-        embed.add_field(name=f"Opened By", value=f"{user.mention}", inline=True)
-        embed.add_field(name=f"Closed By", value=f"{interaction.user.mention}", inline=True)
-        embed.add_field(name=f"Reason", value=f"No reason given", inline=True)
-        embed.set_thumbnail(url=user.avatar.url)
+
+        if user_left:
+            embed.add_field(name=f"Opened By", value=f"{user_id} - User Left", inline=True)
+            embed.add_field(name=f"Closed By", value=f"{interaction.user.mention}", inline=True)
+            embed.add_field(name=f"Reason", value=f"No reason given", inline=True)
+        else:
+            try:
+                embed.add_field(name=f"Opened By", value=f"{user.mention}", inline=True)
+                embed.add_field(name=f"Closed By", value=f"{interaction.user.mention}", inline=True)
+                embed.add_field(name=f"Reason", value=f"No reason given", inline=True)
+                embed.set_thumbnail(url=user.avatar.url)
+            except:
+                return await interaction.response.send_message("Something went wrong while closing this ticket...")
+
         await channel.send(embed=embed)
 
+        self.tickets.update_one(ticket_data, {"$set": {"reason": "No reason given", "status": "CLOSED"}})
+
         await interaction.response.send_message("Closing..")
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.5)
         await interaction.channel.delete()
 
 class InTicketView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
+        self.cluster = MongoClient(MONGODB)
+        self.db = self.cluster["foox"]
+        self.tickets = self.db["tickets"]
     
     @discord.ui.button(label="Close", custom_id="close_ticket", style=discord.ButtonStyle.red, emoji="🔒")
     async def _close_ticket(self, interaction:discord.Interaction, button:discord.ui.Button):
@@ -52,26 +102,35 @@ class InTicketView(discord.ui.View):
     async def _save_ticket(self, interaction:discord.Interaction, button:discord.ui.Button):
         if not interaction.user.guild_permissions.administrator:
             return await interaction.response.send_message("You do not have the required permissions to do that", ephemeral=True)
+    
+        ticket_data = self.tickets.find_one({"channel_id": interaction.channel.id})
+        user_id = ticket_data["user_id"]
+        user = await interaction.guild.fetch_member(user_id)
 
-        await interaction.channel.set_permissions(target=user, overwrite=None)
-
-        await interaction.response.send_message(f"Ticket Saved. Requested By {interaction.user.mention}")
+        if user == None:
+            await interaction.response.send_message(f"I couldn't find the user who opened this ticket, Ticket Saved.")
+        else:
+            await interaction.channel.set_permissions(target=user, overwrite=None)
+            await interaction.response.send_message(f"Ticket Saved. Requested By {interaction.user.mention}")
+        
+        self.tickets.update_one(ticket_data, {"$set": {"status": "SAVED"}})
 
 class TicketView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
+        self.cluster = MongoClient(MONGODB)
+        self.db = self.cluster["foox"]
+        self.tickets = self.db["tickets"]
 
     @discord.ui.button(label="Open A Ticket", custom_id="ticket", style=discord.ButtonStyle.green, emoji="<:FooxGun:1015872747072663592>")
     async def _ticket(self, interaction:discord.Interaction, button:discord.ui.Button):
-        global user
-
-        user = interaction.user
-
         view = InTicketView()
 
         tickets_cg = discord.utils.get(interaction.guild.categories, id=1124373765879496785)
 
         ticket:discord.TextChannel = await tickets_cg.create_text_channel(f"ticket-{interaction.user.name}")
+
+        self.tickets.insert_one({"user_id": interaction.user.id, "channel_id": ticket.id, "guild_id": interaction.guild.id, "reason": "Pending", "status": "OPEN", "user_left": False})
 
         await ticket.set_permissions(interaction.user, read_messages=True, send_messages=True)
         await ticket.set_permissions(interaction.guild.default_role, view_channel=False)
@@ -90,6 +149,7 @@ class Tickets(commands.Cog):
     @commands.command(name='Tickets', aliases=["tickets", "Tk", "tk"], description=f"Shows the drawing list\nUsage:- f!Dc list")
     @commands.has_permissions(administrator=True)
     async def _tickets(self, ctx:commands.Context):
+
         view = TicketView()
         embed = discord.Embed(title="Tickets", description="Open a ticket by using the button below!", color=0xADD8E6)
         embed.set_thumbnail(url=ctx.guild.icon.url)
